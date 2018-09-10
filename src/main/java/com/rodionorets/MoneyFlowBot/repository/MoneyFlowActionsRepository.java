cdb5eff5b4dbe418af7028bbf3a96d210bbfce5a
Rodion Orets <rodiksb@gmail.com>
2018-09-10 16:20:47 +0300

    Custom repository method definition add

--- src/main/java/com/rodionorets/MoneyFlowBot/repository/MoneyFlowActionsRepository.java
+++ src/main/java/com/rodionorets/MoneyFlowBot/repository/MoneyFlowActionsRepository.java
@@ -2,6 +2,13 @@ package com.rodionorets.MoneyFlowBot.repository;
 
 import com.rodionorets.MoneyFlowBot.domain.MoneyFlowAction;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+
+@Repository("moneyFlowActionRepository")
 public interface MoneyFlowActionsRepository extends MongoRepository<MoneyFlowAction, Integer> {
+
+    Collection<MoneyFlowAction> findAllByTelegramUserId(Integer telegramUsedId);
+
 }
