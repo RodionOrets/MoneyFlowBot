009e155ed65edc8881ec83e1a43b01caea8aabd9
Rodion Orets <rodiksb@gmail.com>
2018-09-10 17:59:30 +0300

    GetExpensesActionProcessor implementation add

--- src/main/java/com/rodionorets/MoneyFlowBot/command/implementation/expenses/GetExpensesActionProcessor.java
+++ src/main/java/com/rodionorets/MoneyFlowBot/command/implementation/expenses/GetExpensesActionProcessor.java
@@ -1,15 +1,42 @@
 package com.rodionorets.MoneyFlowBot.command.implementation.expenses;
 
 import com.rodionorets.MoneyFlowBot.command.MoneyFlowActionProcessor;
+import com.rodionorets.MoneyFlowBot.domain.ActionTypes;
+import com.rodionorets.MoneyFlowBot.domain.MoneyFlowAction;
+import com.rodionorets.MoneyFlowBot.repository.MoneyFlowActionsRepository;
 import com.rodionorets.MoneyFlowBot.util.QueriesAndProcessorNames;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+
+import java.math.BigDecimal;
 
 @Service(QueriesAndProcessorNames.Expenses.GET_EXPENSES_ACTION_PROCESSOR_NAME)
 public class GetExpensesActionProcessor extends MoneyFlowActionProcessor {
 
+    private MoneyFlowActionsRepository moneyFlowActionsRepository;
+
+    @Autowired
+    public GetExpensesActionProcessor(MoneyFlowActionsRepository moneyFlowActionsRepository) {
+        this.moneyFlowActionsRepository = moneyFlowActionsRepository;
+    }
+
     @Override
     public void process() {
+        var update = getUpdate();
+        var telegramUserId = update.getInlineQuery().getFrom().getId();
+        var userActions = moneyFlowActionsRepository.findAllByTelegramUserId(telegramUserId);
 
+        var totalUserExpensesAmount = userActions.stream()
+                .filter(action -> action.getActionType().equals(ActionTypes.EXPENSE))
+                .mapToDouble(action -> action.getAmount().doubleValue())
+                .sum();
+
+        String message = "Your total income amount is " + totalUserExpensesAmount;
+
+        SendMessage sendMessage = new SendMessage().setText(message);
+
+        executeApiMethod(sendMessage);
     }
 
 }
