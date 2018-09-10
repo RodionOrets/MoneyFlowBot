a17e378f136a540cf407cb0bff24f7abf8ee7dc9
Rodion Orets <rodiksb@gmail.com>
2018-09-10 18:15:20 +0300

    Refactor: field injection remove, constructor injection add + processor lookup resolving refactor

--- src/main/java/com/rodionorets/MoneyFlowBot/factory/MoneyFlowActionProcessorProvider.java
+++ src/main/java/com/rodionorets/MoneyFlowBot/factory/MoneyFlowActionProcessorProvider.java
@@ -2,50 +2,50 @@ package com.rodionorets.MoneyFlowBot.factory;
 
 import com.rodionorets.MoneyFlowBot.cache.ApplicationCommandCache;
 import com.rodionorets.MoneyFlowBot.command.MoneyFlowActionProcessor;
-import com.rodionorets.MoneyFlowBot.util.QueriesAndProcessorNames;
-import com.rodionorets.MoneyFlowBot.util.ProcessorNameResolver;
+import com.rodionorets.MoneyFlowBot.util.moneyflowbot.ProcessorNameResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.api.objects.Update;
 
-import static com.rodionorets.MoneyFlowBot.util.QueriesAndProcessorNames.General.UNKNOWN_ACTION_PROCESSOR_NAME;
+import static com.rodionorets.MoneyFlowBot.util.moneyflowbot.QueriesAndProcessorNames.General.UNKNOWN_ACTION_PROCESSOR_NAME;
 
 @Service("moneyFlowActionProvider")
 public class MoneyFlowActionProcessorProvider {
 
-    @Autowired
-    private ApplicationContext applicationContext;
+    private final ApplicationContext applicationContext;
+
+    private final ApplicationCommandCache applicationCommandCache;
+
+    private final ProcessorNameResolver processorNameResolver;
 
     @Autowired
-    @Qualifier("applicationCommandCache")
-    private ApplicationCommandCache applicationCommandCache;
-
-    @Autowired
-    @Qualifier("processorNameResolver")
-    private ProcessorNameResolver processorNameResolver;
+    public MoneyFlowActionProcessorProvider(
+            ApplicationContext applicationContext,
+            @Qualifier("applicationCommandCache") ApplicationCommandCache applicationCommandCache,
+            @Qualifier("processorNameResolver") ProcessorNameResolver processorNameResolver) {
+        this.applicationContext = applicationContext;
+        this.applicationCommandCache = applicationCommandCache;
+        this.processorNameResolver = processorNameResolver;
+    }
 
     public MoneyFlowActionProcessor getProcessor(Update update) {
-        var processor = resolveFromContext(UNKNOWN_ACTION_PROCESSOR_NAME, MoneyFlowActionProcessor.class);
-
         if (update.hasInlineQuery()) {
             applicationCommandCache.clear();
-            processor = getProcessorForUpdateWithInlineQuery(update);
+            return getProcessorForUpdateWithInlineQuery(update);
+        } else if (update.hasMessage()) {
+            return null;
+        } else {
+            return resolveFromContext(UNKNOWN_ACTION_PROCESSOR_NAME, MoneyFlowActionProcessor.class);
         }
-        else if (update.hasMessage()) {
-
-        }
-
-        processor.setUpdate(update);
-
-        return processor;
     }
 
     private MoneyFlowActionProcessor getProcessorForUpdateWithInlineQuery(Update update) {
         var query = update.getInlineQuery().getQuery();
         var processorBeanName = processorNameResolver.resolveByQuery(query);
-        return resolveFromContext(processorBeanName, MoneyFlowActionProcessor.class);
+        return resolveFromContext(processorBeanName, MoneyFlowActionProcessor.class)
+                .withUpdate(update);
     }
 
     private <T> T resolveFromContext(String beanName, Class<T> type) {
