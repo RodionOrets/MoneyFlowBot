7542464c37982048767ae81498f0fe146ebd3691
Rodion Orets <rodiksb@gmail.com>
2019-05-14 15:52:50 +0300

    Add basic command processors

---
+++ src/main/java/com/rodionorets/MoneyFlowBot/command/IncomeCommandProcessor.java
@@ -0,0 +1,14 @@
+package com.rodionorets.MoneyFlowBot.command;
+
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Service
+public class IncomeCommandProcessor implements TelegramUpdateProcessor
+{
+    @Override
+    public void process(Update update)
+    {
+
+    }
+}
