e5cafa8d295997f9cd3ab998b5034ed0f107c9a6
Rodion Orets <rodiksb@gmail.com>
2019-05-16 04:22:25 +0300

    add /incomes and /expenses tutor to /help message

--- src/main/java/com/rodionorets/MoneyFlowBot/util/HelpMessages.java
+++ src/main/java/com/rodionorets/MoneyFlowBot/util/HelpMessages.java
@@ -5,23 +5,36 @@ import org.telegram.telegrambots.meta.api.objects.User;
 public final class HelpMessages
 {
     public static final String INCOME_HELP_MESSAGE =
-            "Please use it in form of: /income {amount} {category}\n" +
+        "Please use it in form of: /income {amount} {category}\n" +
             "Example: /income 80000 Salary\n";
 
-    public static final String INCOMES_HELP_MESSAGE = "";
+    private static final String INCOMES_HELP_MESSAGE =
+        "You could use it in next ways:\n" +
+            "/incomes - list of all your incomes\n" +
+            "/incomes {category} - filter by category.\n";
 
     public static final String EXPENSE_HELP_MESSAGE =
-            "Please use it in form of: /expense {amount} {category}\n" +
+        "Please use it in form of: /expense {amount} {category}\n" +
             "Example: /expense 8000 Education\n";
 
-    public static final String EXPENSES_HELP_MESSAGE = "";
+    private static final String EXPENSES_HELP_MESSAGE =
+        "You could use it in next ways:\n" +
+            "/incomes - list of all your incomes\n" +
+            "/incomes {category} - filter by category.\n";
 
     public static final String GLOBAL_HELP_MESSAGE =
-            "At the moment you can use following commands:\n" +
-                    "/income - to save your new income.\n" +
-                    HelpMessages.INCOME_HELP_MESSAGE +
-                    "/expense - to save your new expense.\n" +
-                    HelpMessages.EXPENSE_HELP_MESSAGE;
+        "At the moment you can use following commands:\n" +
+            "/income - to save your new income.\n" +
+            INCOME_HELP_MESSAGE +
+            "/expense - to save your new expense.\n" +
+            EXPENSE_HELP_MESSAGE +
+            "/incomes - returns list of logged incomes. No filters available now.\n" +
+            INCOMES_HELP_MESSAGE +
+            "/expenses - returns list of logger expenses. No filters available now.\n" +
+            EXPENSES_HELP_MESSAGE +
+            "Other commands are:\n" +
+            "/reset - use it to erase all your data,\n" +
+            "/help - to get this help message.\n";
 
     public static final String START_MESSAGE = "I'm Money Flow Bot. I can record your incomes and expenses.\n" + GLOBAL_HELP_MESSAGE;
 
