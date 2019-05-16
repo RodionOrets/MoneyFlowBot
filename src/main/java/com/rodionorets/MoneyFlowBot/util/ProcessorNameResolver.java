c26e043215f81d8395cb1d2bc4c78328433230e5
rodionorets <rodiksb@gmail.com>
2019-05-16 06:30:15 +0300

    move to Java 8

--- src/main/java/com/rodionorets/MoneyFlowBot/util/ProcessorNameResolver.java
+++ src/main/java/com/rodionorets/MoneyFlowBot/util/ProcessorNameResolver.java
@@ -3,27 +3,27 @@ package com.rodionorets.MoneyFlowBot.util;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component
 public class ProcessorNameResolver
 {
-    private final Map<String, String> commandProcessors = Collections.unmodifiableMap(Map
-        .of(
-            "/start", "startCommandProcessor",
-            "/help", "helpCommandProcessor",
-            "/income", "incomeCommandProcessor",
-            "/expense", "expenseCommandProcessor",
-            "/incomes", "incomesCommandProcessor",
-            "/expenses", "expensesCommandProcessor",
-            "/balance", "balanceCommandProcessor",
-            "/reset", "resetCommandProcessor"
-        ));
+    private final Map<String, String> commandProcessors = Stream
+            .of(new String[][]{
+                    {"/start", "startCommandProcessor"},
+                    {"/help", "helpCommandProcessor"},
+                    {"/income", "incomeCommandProcessor"},
+                    {"/expense", "expenseCommandProcessor"},
+                    {"/incomes", "incomesCommandProcessor"},
+                    {"/expenses", "expensesCommandProcessor"},
+                    {"/balance", "balanceCommandProcessor"},
+                    {"/reset", "resetCommandProcessor"}})
+            .collect(Collectors.toMap(d -> d[0], d -> d[1]));
 
-    private final Map<String, String> callbackDataProcessors = Collections.unmodifiableMap(Map
-        .of(
-
-        ));
+    private final Map<String, String> callbackDataProcessors = new HashMap<>();
 
     public String resolveForCommand(String command)
     {
